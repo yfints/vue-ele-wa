2f1e2628c98f27b697f938f32b4eeb7ed687701e
yfx <1249361928@.com>
2026-09-18 14:07:29 +0800

    chore: 优化 TopBar 格式并忽略本地目录

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="headBar flex jb ac" :class="menuOpen ? 'head1' : 'head2'">
     <div class="flex ac flex1">
-      <button type="button" class="menuBox flex ac" aria-label="切换菜单" @click="toggleMenu"  v-if="isPhone">
-        <img src="/clone-assets/menu.png" class="img32" alt="" />
+      <button type="button" class="menuBox flex ac" aria-label="切换菜单" @click="toggleMenu" v-if="isPhone">
+        <img src="/clone-assets/menu.png" class="img32" alt=""/>
       </button>
       <div class="pc flex ac flex1">
         <el-breadcrumb separator="/" aria-label="面包屑">
           <el-breadcrumb-item
-            v-for="(crumb, index) in breadcrumbs"
-            :key="`${crumb.label}-${index}`"
-            :to="index < breadcrumbs.length - 1 ? crumb.to : undefined"
+              v-for="(crumb, index) in breadcrumbs"
+              :key="`${crumb.label}-${index}`"
+              :to="index < breadcrumbs.length - 1 ? crumb.to : undefined"
           >
             {{ crumb.label }}
           </el-breadcrumb-item>
@@ -18,17 +18,17 @@
     </div>
     <div class="flex ac flex0">
       <RouterLink :to="profileTo">
-        <el-image :src="avatarUrl" class="img48 hand circle" fit="cover" />
+        <el-image :src="avatarUrl" class="img48 hand circle" fit="cover"/>
       </RouterLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { useRoute } from "vue-router";
+import {computed} from "vue";
+import {useRoute} from "vue-router";
 import {isPhone, menuOpen, toggleMenu} from "@/composables/useLayout";
-import { avatarUrl, isLoggedIn } from "@/composables/useAuth";
+import {avatarUrl, isLoggedIn} from "@/composables/useAuth";
 
 const route = useRoute();
 
@@ -46,9 +46,9 @@ const breadcrumbs = computed<Crumb[]>(() => {
 });
 
 const profileTo = computed(() =>
-  isLoggedIn.value
-    ? "/home/index"
-    : { path: "/login/index", query: { redirect: route.fullPath } },
+    isLoggedIn.value
+        ? "/home/index"
+        : {path: "/login/index", query: {redirect: route.fullPath}},
 );
 
 </script>
